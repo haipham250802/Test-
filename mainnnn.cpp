6d3da9d9c1b8d2603a64983b8f145f52595b3e02
Phạm Văn Hải <100368878+haipham250802@users.noreply.github.com>
2022-04-07 10:50:35 +0700

    first commit

---
+++ mainnnn.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+cout <<"\t quanh nam buon ban o non song ";
+	cout << " nuoi du nam con voi mot chong";
+	cout << "\tlan noi than co khi quang vang";
+	return 0;
+}
